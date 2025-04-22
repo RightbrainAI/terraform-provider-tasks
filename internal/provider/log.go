f65960611c88ba3e759ae830ffd6a0c0fe6fd3ec
Anthony Sterling <159960+anthonysterling@users.noreply.github.com>
2025-04-22 13:49:28 +0100

    fix: use tf log for sdk logging (#12)

---
+++ internal/provider/log.go
@@ -0,0 +1,35 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+type TerraformLog struct {
+}
+
+func (tl TerraformLog) Debug(msg string, args ...any) {
+	tflog.Debug(context.Background(), msg, tl.argsToMap(args))
+}
+func (tl TerraformLog) Info(msg string, args ...any) {
+	tflog.Info(context.Background(), msg, tl.argsToMap(args))
+}
+func (tl TerraformLog) Warn(msg string, args ...any) {
+	tflog.Warn(context.Background(), msg, tl.argsToMap(args))
+}
+func (tl TerraformLog) Error(msg string, args ...any) {
+	tflog.Error(context.Background(), msg, tl.argsToMap(args))
+}
+func (tl TerraformLog) argsToMap(args ...any) map[string]interface{} {
+	result := make(map[string]any)
+	for i := 0; i < len(args)-1; i += 2 {
+		if key, ok := args[i].(string); ok {
+			result[key] = args[i+1]
+		}
+	}
+	return result
+}
